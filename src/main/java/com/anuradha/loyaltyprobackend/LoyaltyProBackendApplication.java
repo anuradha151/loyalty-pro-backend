fb20bf15ef2eea57391671344fc418d3ef4a5fed
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2024-08-02 15:16:53 +0530

    Initial commit

---
+++ src/main/java/com/anuradha/loyaltyprobackend/LoyaltyProBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anuradha.loyaltyprobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyProBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyProBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anuradha/loyaltyprobackend/LoyaltyProBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuradha.loyaltyprobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyProBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
